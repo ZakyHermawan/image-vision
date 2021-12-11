743b9821687432fa8412c129bde501de781fa6a6
Zaky Hermawan <zaky.hermawan9615@gmail.com>
2021-12-11 18:02:28 +0700

    Interfacing

--- include/image.hpp
+++ include/image.hpp
@@ -4,3 +4,19 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+class Image {
+private:
+  // the image will be stored in hwc format
+  int m_height, m_width, m_channels;
+  float *m_img;
+public:
+  Image() = default;
+  Image(const char* path);
+
+  void make_image(int width, int height, int channels);
+  void load_image(const char* path);
+  void write_image(const char* imgname); 
+  float get_pixel(int x, int y, int c);
+  void set_pixel(int x, int y, int c, float v);
+  ~Image();
+};
--- src/image.cpp
+++ src/image.cpp
@@ -1,40 +1,78 @@
 #include "image.hpp"
+#include <fstream>
+#include <exception>
 
-class Image {
-private:
-  int m_width, m_height, m_channels;
-  unsigned char *m_img;
-public:
-  Image() = delete;
-  Image(const char* path)
-    : m_img(stbi_load(path, &m_width, &m_height, &m_channels, 0)) {}
-  /*
-  {
-    unsigned char* img = stbi_load(path, &m_width, &m_height, &m_channels);
+Image::Image(const char* path) {
+  std::ifstream infile(path);
+  if(!infile.good()) {
+    throw std::runtime_error("File did not exist");
   }
-  */
-  void write_image(const char* imgname) {
-    stbi_write_png(imgname, m_width, m_height, m_channels, m_img, m_width * m_channels);
+  load_image(path);
+}
+
+Image::~Image() {
+  stbi_image_free(m_img);
+}
+
+void Image::write_image(const char* imgname) {
+  unsigned char* img = (unsigned char*)calloc(m_width * m_height * m_channels, sizeof(unsigned char));
+  for(int i=0; i<m_channels; ++i) {
+    for(int j=0; j<m_height; ++j) {
+      for(int k=0; k<m_width; ++k) {
+        int idx = k + m_width * j + m_width * m_height * i;
+        img[idx] = m_img[idx]*255;
+      }
+    }
   }
-  ~Image() {
-    printf("free img\n");
-    stbi_image_free(m_img);
+  stbi_write_png(imgname, m_width, m_height, m_channels, img, m_width * m_channels);
+}
+
+void Image::make_image(int width, int height, int channels) {
+  m_img = (float*) calloc(width * height * channels, sizeof(float));
+  m_width = width;
+  m_height = height;
+  m_channels = channels;
+}
+
+void Image::load_image(const char* path) {
+  // m_img = stbi_load(path, &m_width, &m_height, &m_channels, 0);
+  unsigned char* orig = stbi_load(path, &m_width, &m_height, &m_channels, 0); 
+ 
+  make_image(m_width, m_height, m_channels);
+  
+  // convert to double precision pixel value
+  for(int k=0; k<m_channels; ++k) {
+    for(int j=0; j<m_height; ++j) {
+      for(int i=0; i<m_width; ++i) {
+        int idx = i + m_width * j + m_width * m_height * k;
+        m_img[idx] = (float) orig[idx]/255; 
+      }
+    }
   }
-};
+}
+
+float Image::get_pixel(int x, int y, int c) {
+  if(x < 0 || y < 0) {
+    throw std::runtime_error("Image index cannot be negative");
+  }
+  if(y < 0) {
+    throw std::runtime_error("Channel cannot be negative");
+  }
+  return m_img[y + x * m_width + c * m_width * m_height];
+}
+
+void Image::set_pixel(int x, int y, int c, float v) {
+  if(x < 0 || y < 0) {
+    throw std::runtime_error("Image index cannot be negative");
+  }
+  if(y < 0) {
+    throw std::runtime_error("Channel cannot be negative");
+  }
+  m_img[y + x * m_width + c * m_width * m_height] = v;
+}
 
 int main() {
   Image img("../tests/lenna.png");
   img.write_image("lenna.png");
-  /*
-  unsigned char *img = stbi_load("../tests/lenna.png", &width, &height, &channels, 0);
-  if(img == NULL) {
-    fprintf(stderr, "Error in loading the image\n");
-    exit(1);
-  }
-  printf("Loaded image with width: %d height: %d number of channels %d\n", width, height, channels);
-  stbi_write_png("lenna.png", width, height, channels, img, width*channels);
-  
-  stbi_image_free(img);
-  */
-
+  return 0;
 }
