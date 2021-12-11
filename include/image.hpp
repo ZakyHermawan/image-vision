102a1e66c5a7f711e9db03553f0d9ed04d638de6
Zaky Hermawan <zaky.hermawan9615@gmail.com>
2021-12-12 02:19:27 +0700

    Add grayscaling and fix interfacing

--- include/image.hpp
+++ include/image.hpp
@@ -3,20 +3,35 @@
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#include <memory>
+
+using std::unique_ptr;
+using std::make_shared;
+
+enum ImageType {
+  PNG, JPG, BMP, TGA
+};
 
 class Image {
 private:
   // the image will be stored in hwc format
-  int m_height, m_width, m_channels;
-  float *m_img;
-public:
-  Image() = default;
-  Image(const char* path);
 
+public:
+  int m_height, m_width, m_channels, m_size;
+  unique_ptr<float[]> m_img;
+
+  Image() = default;
+  Image(int width, int height, int channels);
+  Image(const char* path);
+  
+  ImageType get_file_type(const char* filename);
   void make_image(int width, int height, int channels);
+  unique_ptr<unsigned char[]> conv_to_stb();
   void load_image(const char* path);
-  void write_image(const char* imgname); 
+  void write_image(const char* filename);
   float get_pixel(int x, int y, int c);
   void set_pixel(int x, int y, int c, float v);
+  unique_ptr<Image> copy();
+  unique_ptr<Image> grayscale();
   ~Image();
 };
--- src/image.cpp
+++ src/image.cpp
@@ -1,78 +1,184 @@
 #include "image.hpp"
 #include <fstream>
 #include <exception>
+#include <memory>
+#include <algorithm>
+
+using std::unique_ptr;
+using std::make_unique;
+using std::runtime_error;
+
+int main() {
+  unique_ptr<Image> img = make_unique<Image>("../tests/lenna.png");
+  img->write_image("hasil.png");
+  unique_ptr<Image>tmp = img->grayscale();
+  tmp->write_image("gray.png");
+  unique_ptr<Image> tes = make_unique<Image>("../tests/example.jpg");
+  tes->write_image("yow.jpg");
+  unique_ptr<Image> grayscaled = tes->grayscale();
+  grayscaled->write_image("graaaay.jpg");
+  unique_ptr<Image> kopi(grayscaled->copy());
+  kopi->write_image("kopi.png");
+  unique_ptr<Image> colorbar(new Image("../tests/colorbar.png"));
+  auto gray_colorbar(colorbar->grayscale());
+  gray_colorbar->write_image("gray_colorbar.png");
+
+  return 0;
+}
+
+Image::Image(int width, int height, int channels) {
+  make_image(width, height, channels);
+}
 
 Image::Image(const char* path) {
   std::ifstream infile(path);
   if(!infile.good()) {
-    throw std::runtime_error("File did not exist");
+    throw runtime_error("File did not exist");
   }
   load_image(path);
 }
 
-Image::~Image() {
-  stbi_image_free(m_img);
+ImageType Image::get_file_type(const char* filename) {
+  const char* ext = strrchr(filename, '.');
+  if(ext != nullptr) {
+    if(strcmp(ext, ".png") == 0) {
+      return PNG;
+    }
+    else if(strcmp(ext, ".jpg") == 0) {
+      return JPG;
+    }
+    else if(strcmp(ext, ".bmp") == 0) {
+      return BMP;
+    }
+    else if(strcmp(ext, ".tga") == 0) {
+      return TGA;
+    }
+    
+    
+  }
+  throw runtime_error("extension not supported");
+
 }
 
-void Image::write_image(const char* imgname) {
-  unsigned char* img = (unsigned char*)calloc(m_width * m_height * m_channels, sizeof(unsigned char));
+Image::~Image() {
+
+}
+
+
+unique_ptr<unsigned char[]> Image::conv_to_stb() {
+  unique_ptr<unsigned char[]> tmp = make_unique<unsigned char[]>(m_size);
   for(int i=0; i<m_channels; ++i) {
     for(int j=0; j<m_height; ++j) {
       for(int k=0; k<m_width; ++k) {
-        int idx = k + m_width * j + m_width * m_height * i;
-        img[idx] = m_img[idx]*255;
+        size_t src_idx = k + j * m_width + i * m_width * m_height;
+        size_t dst_idx = i + k * m_channels + j * m_channels * m_width;
+        tmp[dst_idx] = (unsigned char)roundf(m_img[src_idx] * 255);
       }
     }
   }
-  stbi_write_png(imgname, m_width, m_height, m_channels, img, m_width * m_channels);
+  return tmp;
+}
+
+void Image::write_image(const char* filename) {
+  ImageType type = get_file_type(filename);
+  unique_ptr<unsigned char[]> conv = conv_to_stb();
+  switch(type) {
+    case PNG:
+      stbi_write_png(filename, m_width, m_height, m_channels, conv.get(), m_width * m_channels);
+      break;
+    case BMP:
+      stbi_write_bmp(filename, m_width, m_height, m_channels, conv.get());
+      break;
+    case JPG:
+      stbi_write_jpg(filename, m_width, m_height, m_channels, conv.get(), 100);
+      break;
+    case TGA:
+      stbi_write_tga(filename, m_width, m_height, m_channels, conv.get());
+  }
 }
 
 void Image::make_image(int width, int height, int channels) {
-  m_img = (float*) calloc(width * height * channels, sizeof(float));
   m_width = width;
   m_height = height;
   m_channels = channels;
+  m_size = m_width * m_height * m_channels;
+  m_img = make_unique<float[]>(m_size);
 }
 
 void Image::load_image(const char* path) {
-  // m_img = stbi_load(path, &m_width, &m_height, &m_channels, 0);
-  unsigned char* orig = stbi_load(path, &m_width, &m_height, &m_channels, 0); 
- 
-  make_image(m_width, m_height, m_channels);
-  
-  // convert to double precision pixel value
-  for(int k=0; k<m_channels; ++k) {
+  unsigned char* tmp = stbi_load(path, &m_width, &m_height, &m_channels, 0);
+  m_size = m_width * m_height * m_channels;
+  m_img = make_unique<float[]>(m_size);
+
+  for(int i=0; i<m_channels; ++i) {
     for(int j=0; j<m_height; ++j) {
-      for(int i=0; i<m_width; ++i) {
-        int idx = i + m_width * j + m_width * m_height * k;
-        m_img[idx] = (float) orig[idx]/255; 
+      for(int k=0; k<m_width; ++k) {
+        int dst_index = k + j * m_width + i * m_width * m_height;
+        int src_index = i + m_channels * k + m_channels * m_width * j;
+        m_img[dst_index] = (float)tmp[src_index]/255;
       }
     }
   }
+  stbi_image_free(tmp);
 }
+
 
 float Image::get_pixel(int x, int y, int c) {
   if(x < 0 || y < 0) {
-    throw std::runtime_error("Image index cannot be negative");
+    throw runtime_error("Image index cannot be negative");
   }
   if(y < 0) {
-    throw std::runtime_error("Channel cannot be negative");
+    throw runtime_error("Channel cannot be negative");
   }
-  return m_img[y + x * m_width + c * m_width * m_height];
+  return m_img[x + y * m_width + c * m_width * m_height];
 }
+
 
 void Image::set_pixel(int x, int y, int c, float v) {
   if(x < 0 || y < 0) {
-    throw std::runtime_error("Image index cannot be negative");
+    throw runtime_error("Image index cannot be negative");
   }
   if(y < 0) {
-    throw std::runtime_error("Channel cannot be negative");
+    throw runtime_error("Channel cannot be negative");
   }
-  m_img[y + x * m_width + c * m_width * m_height] = v;
+  m_img[x + y * m_width + c * m_width * m_height] = v;
 }
 
-int main() {
-  Image img("../tests/lenna.png");
-  img.write_image("lenna.png");
-  return 0;
+
+unique_ptr<Image> Image::copy() {
+  unique_ptr<Image> new_img(new Image(m_width, m_height, m_channels));
+  for(int i=0; i<m_channels; ++i) {
+    for(int j=0; j<m_height; ++j) {
+      for(int k=0; k<m_width; ++k) {
+        new_img->set_pixel(k, j, i, get_pixel(k, j, i));
+      }
+    }
+  }
+  return new_img;
 }
+
+unique_ptr<Image> Image::grayscale() {
+  if(this->m_channels != 3) {
+    throw runtime_error("You must have three channels");
+  }
+
+  unique_ptr<Image> gray(new Image(m_width, m_height, 1));
+
+  float wred = 0.299;
+  float wgreen = 0.587;
+  float wblue = 0.114;
+
+  int ctr = 0;
+  for(int i=0; i<m_height; ++i) {
+    for(int j=0; j<m_width; ++j) {
+      float red, green, blue, greyval;
+      red = get_pixel(j, i, 0);
+      green = get_pixel(j, i, 1);
+      blue = get_pixel(j, i, 2);
+      greyval = red * wred + green * wgreen + blue * wblue;
+      gray->set_pixel(j, i, 0, greyval);
+    }
+  }
+  return gray;
+}
+
